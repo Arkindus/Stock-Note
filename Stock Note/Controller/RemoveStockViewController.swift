230a784cea097e05f824e83f56323dd36d527961
Ankit <ankit.rk.ca@gmail.com>
2020-10-11 09:27:52 +0530

    Make datePicker and stockPicker fetch and show data

--- Stock Note/Controller/RemoveStockViewController.swift
+++ Stock Note/Controller/RemoveStockViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import RealmSwift
 
 class RemoveStockViewController: UIViewController {
+    
+    let realm = try! Realm()
+    var stocks: Results<Stock>?
+    var entries: Results<Entry>?
     
     let dateFormat = DateFormat()
     
@@ -27,18 +32,48 @@ class RemoveStockViewController: UIViewController {
         rateTextField.layer.borderWidth = 0.75
         rateTextField.layer.borderColor = #colorLiteral(red: 1, green: 0.04984947294, blue: 0.09696806222, alpha: 1)
         rateTextField.layer.cornerRadius = 5
+        
+        loadPickerView()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        stockPicker.delegate = self
         
         if #available(iOS 13.4, *) {
             datePicker.preferredDatePickerStyle = .wheels // Replace .inline with .compact
         }
     }
     
+    //MARK: - Date PickerView Methods
     @IBAction func datePickerChanged(_ sender: UIDatePicker) {
         datePicker = sender
         print(datePicker.date)
     }
+    
+    //MARK: - Data Manipulation Methods
+    func loadPickerView() {
+        stocks = realm.objects(Stock.self).sorted(byKeyPath: K.dateUpdated, ascending: false)
+        stockPicker.reloadAllComponents()
+    }
+}
+
+//MARK: - PickerView Datasource and Delegate Methods
+extension RemoveStockViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stocks?.count ?? 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return stocks?[row].name ?? "None"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(stocks?[row].name)
+    }
+   
 }
